6f786689f092c1d3ead3dfc5fa240ba77c1dd448
dilee <dilee@lunasoft.co.kr>
2018-01-31 10:17:12 +0900

    version up

--- Dilee/Package/Hello.php
+++ Dilee/Package/Hello.php
@@ -3,6 +3,6 @@ namespace Dilee\Package;
 
 class Hello {
     public function world() {
-        return 'Hello world package2 version 1.0';
+        return 'Hello world package2 version 1.0.2';
     }
 }
